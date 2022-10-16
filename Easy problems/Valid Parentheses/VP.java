7f4387c244de55d487c768c121f3d0525d292f96
FernandoHenning <fernando_hema@hotmail.com>
2022-10-15 22:55:30 -0500

    Valid Parentheses solved

--- Easy problems/Valid Parentheses/VP.java
+++ Easy problems/Valid Parentheses/VP.java
@@ -1,19 +1,45 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class VP {
     public static void main(String[] args) {
-        String s = "(]";
+        String s = "((";
         System.out.println(isValid(s));
     }
 
     public static  boolean isValid(String s) {
-        long openParenthesesCount = s.chars().filter(ch -> ch == '(').count();
-        long closeParenthesesCount = s.chars().filter(ch -> ch == ')').count();
+        Stack<Character> leftSymbols = new Stack<>();
+        List<Character> openSymbols =  Arrays.asList('(','[','{');
+        if(s.length()>1){
+            for (Character ch : s.toCharArray()) {   
+                if(openSymbols.contains(ch)){
+                    leftSymbols.add(ch);
+                }else{
+                    switch (ch) {
+                        case ')':
+                            if(!leftSymbols.isEmpty() && leftSymbols.peek() == '('){
+                                 leftSymbols.pop();
+                            }else{return false;}
+                            break;
+                        case ']':
+                            if(!leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+                                leftSymbols.pop();
+                            }else{return false;}
+                            break;
+                        case '}':
+                            if(!leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+                                leftSymbols.pop();
+                            }else{return false;}
+                            break;
+                        default:
+                            return false;
+                    }
+                }
+    
+            }
+        }else return false;
 
-        long openSquareBracketsCount = s.chars().filter(ch -> ch == '[').count();
-        long closeSquareBracketsCount = s.chars().filter(ch -> ch == ']').count();
-
-        long openBracesCount = s.chars().filter(ch -> ch == '[').count();
-        long closeBracesCount = s.chars().filter(ch -> ch == ']').count();
-
-        return openParenthesesCount == closeParenthesesCount && openSquareBracketsCount == closeSquareBracketsCount && openBracesCount == closeBracesCount;
+        return leftSymbols.isEmpty();
     }
 }
