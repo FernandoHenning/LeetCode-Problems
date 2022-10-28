312abe88a7536c467efba6a4e920cace6a3a610f
FernandoHenning <fernando_hema@hotmail.com>
2022-10-27 19:17:57 -0500

    Refactored initial conditionals

--- Easy problems/Merge Two Sorted Lists/MTSL.java
+++ Easy problems/Merge Two Sorted Lists/MTSL.java
@@ -1,28 +1,27 @@
 
 class MTSL {
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(1);
-        ListNode list2 = new ListNode(1);
+        ListNode list1 = new ListNode(-6);
+        ListNode list2 = new ListNode(0);
 
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(4);
+        list1.next = new ListNode(-5);
+        list1.next.next = new ListNode(6);
 
-        list2.next = new ListNode(3);
-        list2.next.next = new ListNode(4);
+        list1.next.next.next = new ListNode(6);
+        list1.next.next.next.next = new ListNode(7);
         ListNode head = mergeTwoLists(list1, list2);
         printList(head);
-        
     }
 
     static public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1.val == 0 && list2.val == 0) {
-            return null;
-        }else if(list1.val == 0){
-            return list2;
-        }else if(list2.val == 0){
+        if(list2 == null && list1 == null) return null;
+        if(list1 != null && list2 == null){
             return list1;
-            
         }
+        if(list2 !=null && list1 == null){
+            return list2;
+        }
+
         ListNode newHead = new ListNode();
         ListNode temp = newHead;
         do {
