12c9302cc7a858fc011f741e485da89199c313eb
FernandoHenning <fernando_hema@hotmail.com>
2022-11-02 17:25:40 -0600

    Remove element solved

--- Easy problems/Remove Element/RE.java
+++ Easy problems/Remove Element/RE.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class RE {
     public static void main(String[] args) {
-
+        int[] nums = {2};
+        int val = 3;
+        int k = removeElement(nums, val);
+        System.out.println("K: " + k);
+        System.out.println("Nums :" + Arrays.toString(nums));
+    }
+    public static int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        int k = 0;
+        if(length == 0) return 0;
+        if (length == 1) {
+            return nums[0] != val ? ++k : k;
+        }
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
 }
