e61c8860a6c6f6e487f368bdbac76f940a80d171
fernando.henning <fernando.henning@vantis.mx>
2022-11-21 15:06:45 -0600

    Add Binary problem started

---
+++ Easy problems/Add Binary/AB.java
@@ -0,0 +1,17 @@
+public class AB {
+    public static void main(String[] args) {
+
+    }
+
+   /***
+    0 + 0 = 0
+    0 + 1 = 1
+    1 + 0 = 1
+    1 + 1 = 0, carry over the 1
+    ***/
+    String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+
+        return "";
+    }
+}
