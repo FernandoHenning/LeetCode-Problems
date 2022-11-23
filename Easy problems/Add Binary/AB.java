ca8ad216d106349120a82a9583ec3c56d5636509
fernando.henning <fernando.henning@vantis.mx>
2022-11-22 23:09:34 -0600

    Add Binary problem solved

--- Easy problems/Add Binary/AB.java
+++ Easy problems/Add Binary/AB.java
@@ -1,17 +1,28 @@
 public class AB {
     public static void main(String[] args) {
-
+        System.out.println(addBinary("1011010", "1011"));
     }
 
-   /***
+   /***+
     0 + 0 = 0
     0 + 1 = 1
     1 + 0 = 1
     1 + 1 = 0, carry over the 1
     ***/
-    String addBinary(String a, String b) {
-        StringBuilder result = new StringBuilder();
-
-        return "";
+    static String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+        }
+        if(carry != 0) res.append(carry);
+        return res.reverse().toString();
     }
 }
+
